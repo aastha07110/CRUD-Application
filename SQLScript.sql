1314f673ce201ac0b307dbba9579d1c1d95de197
aastha07110 <sha.aastha07@gmail.com>
2026-01-09 00:52:52 +0530

    Create SQLScript.sql

---
+++ SQLScript.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE foodiedb;
+USE foodiedb;
+
+CREATE TABLE Users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    fullname VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO Users (username, password, fullname, email)
+VALUES ('admin', 'admin123', 'Admin User', 'admin@example.com');
