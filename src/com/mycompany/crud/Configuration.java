7ab05fa6a05fd38f8cda1c28da2ec237e70e514a
aastha07110 <sha.aastha07@gmail.com>
2026-01-09 02:11:35 +0530

    Update Configuration.java

--- src/com/mycompany/crud/Configuration.java
+++ src/com/mycompany/crud/Configuration.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 
 public class Configuration {
 
-    private final static String URL = "jdbc:mysql://localhost:3306/foodiedb";
+    private final static String URL = "jdbc:mysql://localhost:3306/finalproject";
     private final static String USERNAME = "root";
-    private final static String PASSWORD = "123456789";
+    private final static String PASSWORD = "";
 
     public static Connection createConnection() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (Exception ex) {
             ex.printStackTrace();
