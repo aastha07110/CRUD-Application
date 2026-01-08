0831e3bb97823dc423fcf5054f19b5a420ba70f9
aastha07110 <sha.aastha07@gmail.com>
2026-01-09 01:00:12 +0530

    Add Main.java

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -1,1 +1,16 @@
+package com.mycompany;
+
+import com.mycompany.crud.DeleteUser;
+import com.mycompany.crud.UpdateUser;
+import com.mycompany.crud.UserCrudOperation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserCrudOperation userCrudOperation = new UserCrudOperation();
+
+        // Get all users
+        userCrudOperation.getAllUsers();
+    }
+}
 
